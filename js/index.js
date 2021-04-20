6d116f2badc49f75eb17e9d015d3147ce9c55ced
tony <anthonyalcarazoreyes@gmail.com>
2021-04-20 09:16:49 -0500

    pag 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  var date = bulmaCalendar.attach('[type="date"]');
-  var time = bulmaCalendar.attach('[type="time"]');
+  $('#date').datetimepicker({
+    ownerDocument: document,
+    contentWindow: window,
+    timepicker: false,
+    value:'',
+    mask:false,
+
+  });
+
+  $('#time').datetimepicker({
+    datepicker: false,
+  });
 
 });
